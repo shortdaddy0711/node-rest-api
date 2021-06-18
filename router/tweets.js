9922c933574cbb28f9d36f2d5d261b0e8d97163b
shortdaddy0711 <sodoatm@gmail.com>
2021-06-18 16:08:58 -0700

    added validation for text with express-validator

--- router/tweets.js
+++ router/tweets.js
@@ -1,15 +1,25 @@
 import express from 'express';
+import { body } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 import controller from '../controller/tweets.js';
 
 const router = express.Router();
+
+const validateTweet = [
+    body('text')
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage('Minimum length of text is 3'),
+    validate,
+];
 
 router.get('/', controller.getList);
 
 router.get('/:id', controller.getById);
 
-router.post('/', controller.addTweet);
+router.post('/', validateTweet, controller.addTweet);
 
-router.put('/:id', controller.updateTweet);
+router.put('/:id', validateTweet, controller.updateTweet);
 
 router.delete('/:id', controller.deleteTweet);
 
