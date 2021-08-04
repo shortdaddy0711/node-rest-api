afb161cad3f522e14edd000933d7a8144793d3f6
shortdaddy0711 <sodoatm@gmail.com>
2021-08-03 22:48:17 -0700

    finished adding websocket to sync messages of all users on the network

--- app.js
+++ app.js
@@ -3,16 +3,12 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import helmet from 'helmet';
-import { createServer } from 'http';
 import tweetsRoute from './router/tweets.js';
 import authRoute from './router/auth.js';
 import { config } from './config.js';
-import initSocket from './connection/socket.js';
+import { initSocket } from './connection/socket.js';
 
 const app = express();
-const httpServer = createServer(app);
-
-app.set('port', config.host.port || 8080);
 
 app.use(cors());
 app.use(express.json());
@@ -24,30 +20,17 @@ app.use(helmet());
 app.use('/api/tweets', tweetsRoute);
 app.use('/api/auth', authRoute);
 
-app.use(logErrors);
-app.use(clientErrorHandler);
-app.use(errorHandler);
-
-function logErrors(err, req, res, next) {
-	console.error(err.stack);
-	next(err);
-}
-
-function clientErrorHandler(err, req, res, next) {
-	if (req.xhr) {
-		res.status(500).send({ error: 'Something failed!' });
-	} else {
-		next(err);
-	}
-}
-
-function errorHandler(err, req, res, next) {
-	res.status(500);
-	res.render('error', { error: err });
-}
-
-httpServer.listen(app.get('port'), function () {
-	console.log('Express server listening on port ' + app.get('port'));
+app.use((req, res, next) => {
+	res.sendStatus(404);
 });
 
-initSocket(httpServer);
+app.use((error, req, res, next) => {
+	console.error(error);
+	res.sendStatus(500);
+});
+
+const server = app.listen(config.host.port, () => {
+	console.log('Express server listening on port ' + config.host.port);
+});
+
+initSocket(server);
